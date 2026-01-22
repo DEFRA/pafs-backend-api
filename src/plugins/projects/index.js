657c6c08e5640753ae3ee96c944379f1e283af15
Sajir Khan <sajir.khan@cognizant.com>
2026-01-22 18:06:14 +0000

    Fetch proposal data for proposal overview page

--- src/plugins/projects/index.js
+++ src/plugins/projects/index.js
@@ -1,13 +1,15 @@
 import checkProjectName from './check-project-name/check-project-name.js'
+import getProjectOverview from './read-project-overview/read-project-overview.js'
 
 const projectsPlugin = {
   name: 'projects',
   version: '1.0.0',
   register: (server, _options) => {
-    server.route([checkProjectName])
+    server.route([checkProjectName, getProjectOverview])
     server.logger.info('Projects plugin registered')
   }
 }
 
 export default projectsPlugin
 export { default as checkProjectName } from './check-project-name/check-project-name.js'
+export { default as getProjectOverview } from './read-project-overview/read-project-overview.js'
--- src/plugins/projects/services/project-service.js
+++ src/plugins/projects/services/project-service.js
@@ -42,4 +42,49 @@ export class ProjectService {
       throw error
     }
   }
+
+  /**
+   * Get Project Overview data
+   */
+  async getProjectOverviewByReferenceNumber(referenceNumber) {
+    if (!referenceNumber || referenceNumber.length === 0) {
+      return []
+    }
+
+    this.logger.info(
+      { referenceNumber },
+      'Fetching project details by reference number'
+    )
+
+    const projectProposal = await this.prisma.pafs_core_projects.findFirst({
+      where: {
+        reference_number: referenceNumber
+      },
+      select: {
+        reference_number: true,
+        name: true,
+        rma_name: true,
+        project_type: true,
+        project_intervention_types: true,
+        main_intervention_type: true,
+        earliest_start_year: true,
+        project_end_financial_year: true,
+        updated_at: true
+      }
+    })
+
+    const proposalData = {
+      referenceNumber: projectProposal.reference_number,
+      projectName: projectProposal.name,
+      rmaArea: projectProposal.rma_name,
+      projectType: projectProposal.project_type,
+      interventionTypes: projectProposal.project_intervention_types.split(','),
+      mainInterventionType: projectProposal.main_intervention_type,
+      startYear: Number(projectProposal.earliest_start_year),
+      endYear: Number(projectProposal.project_end_financial_year),
+      lastUpdated: projectProposal.updated_at
+    }
+
+    return proposalData
+  }
 }
